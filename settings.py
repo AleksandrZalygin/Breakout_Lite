6c1a88d677245cb54a80fcf133b71abdf5af8b10
Varyamo123 <78637154+Varyamo123@users.noreply.github.com>
2021-02-09 16:25:01 +0800

    Настройки приложения

---
+++ settings.py
@@ -0,0 +1,27 @@
+# base
+GAME_NAME = 'Breakout Lite'
+
+COLOR_CANVAS = "black"
+
+WIDTH = 500
+HEIGHT = 500
+
+#ball
+COLOR_BALL = 'green'
+HEIGHT_BALL = 30
+WIDTH_BALL = 15
+
+#paddle
+COLOR_PADDLE = 'red'
+HEIGHT_PADDLE = 100
+WIDTH_PADDLE = 10
+
+# score
+COLOR_SCORE = 'white'
+HEIGHT_TEXT_SCORE = 450
+WIDTH_TEXT_SCORE = 10
+
+# endgame
+COLOR_END_TEXT = 'yellow'
+HEIGHT_END_TEXT = 250
+WIDTH_END_TEXT = 120
